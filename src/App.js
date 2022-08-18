0b03dd43202128be8eb33bdaa7f4d83b0378df67
Kofeinich <agentkonch@mail.ru>
2022-08-18 16:35:41 +0300

    feat: started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import './styles/css/reset.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <h1 className="title">
+            To Do List
+        </h1>
+        <div className='work-space'>
+            <div className="toolbar">
+                <button className="button">
+                    Add Task
+                </button>
+                <form className="search">
+                    <input type="text" placeholder="Искать здесь..."/>
+                    <button  type="submit">
+
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
   );
 }
